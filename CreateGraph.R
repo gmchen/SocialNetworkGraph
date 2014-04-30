dd893ebed5f1af1fca89980d38a0755ed5e5f9ec
gmchen <gregorymchen@gmail.com>
2014-04-30 15:14:24 -0400

    Added initial code file

---
+++ CreateGraph.R
@@ -0,0 +1,8 @@
+require(Rfacebook)
+require(igraph)
+
+# First, we need to set up a Facebook app as described in 
+# http://thinktostart.wordpress.com/2013/11/19/analyzing-facebook-with-r/
+
+# fb_oauth <- fbOAuth(app_id="", app_secret="")
+# save(fb_oauth, file="fb_oauth")
